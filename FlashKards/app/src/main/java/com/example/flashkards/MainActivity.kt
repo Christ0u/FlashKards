40829d901e25298aad39a2c156076ae2835f63c4
Christopher GERARD <christopher.gerard@ikmail.com>
2025-06-18 22:00:44 +0200

    CHG : Modification du nom du thème par le nom de l'application
    Signed-off-by: Christopher GERARD <christopher.gerard@ikmail.com>

--- FlashKards/app/src/main/java/com/example/flashkards/MainActivity.kt
+++ FlashKards/app/src/main/java/com/example/flashkards/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import com.example.flashkards.ui.theme.TP_FlashcardTheme
+import com.example.flashkards.ui.theme.FlashKards
 
 class MainActivity : ComponentActivity () {
     private val homeViewModel : HomeViewModel by viewModels ()
     override fun onCreate ( savedInstanceState : Bundle ?) {
         super . onCreate ( savedInstanceState )
         setContent {
-            TP_FlashcardTheme {
+            FlashKards {
                 FlashcardNavHost ( homeViewModel = homeViewModel )
             }
         }
